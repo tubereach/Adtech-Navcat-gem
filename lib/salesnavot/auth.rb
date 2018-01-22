ed5215b2d5c8f71e0dc40610c9e7749df44fb85c
Julien Vanderstock <julien.vanderstock@openmailbox.org>
2018-01-22 09:21:27 +0100

    Some verification to login added

--- lib/salesnavot/auth.rb
+++ lib/salesnavot/auth.rb
@@ -14,40 +14,47 @@ module Salesnavot
       puts "click login"
       @session.find('#identifierNext').click
 
-      # After that we fill password, why a while ?
-      #while @session.all('#stream-container').count == 0
-        puts "waiting for login"
-        sleep(0.2)
-      #end
+      puts "waiting for password field"
+      sleep(0.2)
+
       @session.fill_in "password", with: password
       @session.find('#passwordNext').click
 
-      # Sometimes we're asked to verify things
-      # p @session.all('#headingText')
-      # p @session.find('#headingText').text
-      # @session.fill_in "knowledgeLoginLocationInput", with: "Nantes"
-      # @session.find('#next').click
-      # if "Verify it's you"
-      #   do stuffsales_nav_url:
-      # else
-      #   do other stuff
-      # end
+      sleep(1)
+      current_url = @session.driver.current_url()
 
-      #Goto adwords
-      sleep(0.2)
-      @session.visit("https://adwords.google.com/um/signin?hl=fr&sourceid=awo&subid=fr-ww-di-g-aw-a-awhp_1!o2")
-      sleep(0.2)
-      @session.fill_in "password", with: password
-      sleep(0.2)
-      @session.find('#passwordNext').click
-      sleep(0.2)
-      campaign_test = Salesnavot::Campaign.new(
-        {sales_nav_url: "https://adwords.google.com/um/identity?authuser=0&dst=/um/homepage?__e%3D6671079794"},
-        @session)
-      campaign_test.scrap
+      # Sometimes login is more complex
+      if current_url.include?("myaccount.google.com")
+        puts "On my account page, connecting to adwords..."
+        continue_to_adwords = true
+      end
 
+      # Goto adwords
+      if continue_to_adwords
+        @session.visit("https://adwords.google.com/um/signin?hl=fr&sourceid=awo&subid=fr-ww-di-g-aw-a-awhp_1!o2")
+        sleep(1)
+        current_url = @session.driver.current_url();
+        if current_url.include?("GlifWebSignIn")
+          @session.fill_in "password", with: password
+          @session.find('#passwordNext').click
+          sleep(1)
+        end
+
+        sleep(1)
+        current_url = @session.driver.current_url();
+        if current_url.include?("Welcome")
+          # select a where title = "TUBEREACH - Test (Okay)"
+          puts "todo"
+        end
+
+        # campaign_test = Salesnavot::Campaign.new(
+        #   {sales_nav_url: "https://adwords.google.com/um/identity?authuser=0&dst=/um/homepage?__e%3D6671079794"},
+        #   @session)
+        # campaign_test.scrap
+      else
+        puts "A step must be missing"
+      end
 
     end
-
   end
 end
